083ca29612e64b2c48f6b649ea54fcfcab9d2c52
agrublev <agrublev@gmail.com>
2019-12-18 14:02:49 -0800

    upd

--- webpack.config.js
+++ webpack.config.js
@@ -1,71 +1,75 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
-const {less} = require("svelte-preprocess-less");
+const { less } = require("svelte-preprocess-less");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const mode = process.env.NODE_ENV || "development";
 const prod = mode === "production";
 
 module.exports = {
-  entry: {
-    bundle: ["./src/index.js"]
-  },
-  resolve: {
-    alias: {
-      svelte: path.resolve("node_modules", "svelte")
+    entry: {
+        bundle: ["./src/index.js"]
     },
-    extensions: [".mjs", ".js", ".svelte"],
-    mainFields: ["svelte", "browser", "module", "main"]
-  },
-  output: {
-    path: __dirname + "/public",
-    filename: "[name].js",
-    chunkFilename: "[name].[id].js"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.svelte$/,
-        use: {
-          loader: "svelte-loader",
-          options: {
-            emitCss: true,
-            hotReload: true,
-            style: less()
-          }
-        }
-      },
-      {
-        test: /\.css$/,
-        use: [
-          /**
-           * MiniCssExtractPlugin doesn't support HMR.
-           * For developing, use 'style-loader' instead.
-           * */
-          prod ? MiniCssExtractPlugin.loader : "style-loader",
-          "css-loader"
+    resolve: {
+        alias: {
+            svelte: path.resolve("node_modules", "svelte")
+        },
+        extensions: [".mjs", ".js", ".svelte"],
+        mainFields: ["svelte", "browser", "module", "main"]
+    },
+    output: {
+        path: __dirname + "/dist",
+        filename: "[name].js",
+        chunkFilename: "[name].[id].js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.svelte$/,
+                use: {
+                    loader: "svelte-loader",
+                    options: {
+                        emitCss: true,
+                        hotReload: true,
+                        style: less()
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    /**
+                     * MiniCssExtractPlugin doesn't support HMR.
+                     * For developing, use 'style-loader' instead.
+                     * */
+                    prod ? MiniCssExtractPlugin.loader : "style-loader",
+                    "css-loader"
+                ]
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    },
+                    {
+                        loader: "css-loader"
+                    },
+                    {
+                        loader: "less-loader"
+                    }
+                ]
+            }
         ]
-      },
-      {
-        test: /\.less$/,
-        use: [
-          {
-            loader: "style-loader"
-          },
-          {
-            loader: "css-loader"
-          },
-          {
-            loader: "less-loader"
-          }
-        ]
-      }
-    ]
-  },
-  mode,
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: "[name].css"
-    })
-  ],
-  devtool: prod ? false : "source-map"
+    },
+    mode,
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].css"
+        }),
+        new HtmlWebpackPlugin({
+            title: "AWESOME SVET:E<"
+        })
+    ],
+    devtool: prod ? false : "source-map"
 };
